6e9045ebef587479aabf6ca457279a2a9099f1b1
martinGlvn <lkn.galvan.martin@gmail.com>
2023-07-04 16:57:03 -0300

    dependencies & init configs

--- backend/index.ts
+++ backend/index.ts
@@ -1,2 +1,13 @@
-let hi = "hola";
-console.log(hi);
+// imports
+import "dotenv/config";
+import express from "express";
+
+// sv express
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hi!");
+});
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log("app:", PORT);
+});
